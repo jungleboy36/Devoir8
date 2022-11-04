de15d101e25014c51538e44d3507cea068bacc2a
jungleboy36 <93709766+jungleboy36@users.noreply.github.com>
2022-11-04 09:21:49 +0100

    Add files via upload

---
+++ src/app/add-produit/add-produit.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
+@Component({
+  selector: 'app-add-produit',
+  templateUrl: './add-produit.component.html'
+})
+export class AddProduitComponent implements OnInit {
+
+  newProduit = new Produit();
+  constructor(private produitService: ProduitService,private router : Router) { }
+
+  ngOnInit(): void {
+  }
+
+  addProduit(){
+    this.produitService.ajouterProduit(this.newProduit)
+    .subscribe(prod => {
+    console.log(prod);
+    });
+    this.router.navigate(['produits']);
+    }
+
+}
